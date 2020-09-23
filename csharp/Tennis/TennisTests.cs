05a3cdca7079547df0580dd1de259c09c91e5bc4
Tom Stuttard <tom.e.stuttard@gmail.com>
2020-09-23 21:58:34 +0100

    Formatting

--- csharp/Tennis/TennisTests.cs
+++ csharp/Tennis/TennisTests.cs
@@ -3,37 +3,37 @@ using NUnit.Framework;
 
 namespace Tennis
 {
-    [TestFixture( 0,  0, "Love-All")]
-    [TestFixture( 1,  1, "Fifteen-All")]
-    [TestFixture( 2,  2, "Thirty-All")]
-    [TestFixture( 3,  3, "Deuce")]
-    [TestFixture( 4,  4, "Deuce")]
-    [TestFixture( 1,  0, "Fifteen-Love")]
-    [TestFixture( 0,  1, "Love-Fifteen")]
-    [TestFixture( 2,  0, "Thirty-Love")]
-    [TestFixture( 0,  2, "Love-Thirty")]
-    [TestFixture( 3,  0, "Forty-Love")]
-    [TestFixture( 0,  3, "Love-Forty")]
-    [TestFixture( 4,  0, "Win for player1")]
-    [TestFixture( 0,  4, "Win for player2")]
-    [TestFixture( 2,  1, "Thirty-Fifteen")]
-    [TestFixture( 1,  2, "Fifteen-Thirty")]
-    [TestFixture( 3,  1, "Forty-Fifteen")]
-    [TestFixture( 1,  3, "Fifteen-Forty")]
-    [TestFixture( 4,  1, "Win for player1")]
-    [TestFixture( 1,  4, "Win for player2")]
-    [TestFixture( 3,  2, "Forty-Thirty")]
-    [TestFixture( 2,  3, "Thirty-Forty")]
-    [TestFixture( 4,  2, "Win for player1")]
-    [TestFixture( 2,  4, "Win for player2")]
-    [TestFixture( 4,  3, "Advantage player1")]
-    [TestFixture( 3,  4, "Advantage player2")]
-    [TestFixture( 5,  4, "Advantage player1")]
-    [TestFixture( 4,  5, "Advantage player2")]
+    [TestFixture(0, 0, "Love-All")]
+    [TestFixture(1, 1, "Fifteen-All")]
+    [TestFixture(2, 2, "Thirty-All")]
+    [TestFixture(3, 3, "Deuce")]
+    [TestFixture(4, 4, "Deuce")]
+    [TestFixture(1, 0, "Fifteen-Love")]
+    [TestFixture(0, 1, "Love-Fifteen")]
+    [TestFixture(2, 0, "Thirty-Love")]
+    [TestFixture(0, 2, "Love-Thirty")]
+    [TestFixture(3, 0, "Forty-Love")]
+    [TestFixture(0, 3, "Love-Forty")]
+    [TestFixture(4, 0, "Win for player1")]
+    [TestFixture(0, 4, "Win for player2")]
+    [TestFixture(2, 1, "Thirty-Fifteen")]
+    [TestFixture(1, 2, "Fifteen-Thirty")]
+    [TestFixture(3, 1, "Forty-Fifteen")]
+    [TestFixture(1, 3, "Fifteen-Forty")]
+    [TestFixture(4, 1, "Win for player1")]
+    [TestFixture(1, 4, "Win for player2")]
+    [TestFixture(3, 2, "Forty-Thirty")]
+    [TestFixture(2, 3, "Thirty-Forty")]
+    [TestFixture(4, 2, "Win for player1")]
+    [TestFixture(2, 4, "Win for player2")]
+    [TestFixture(4, 3, "Advantage player1")]
+    [TestFixture(3, 4, "Advantage player2")]
+    [TestFixture(5, 4, "Advantage player1")]
+    [TestFixture(4, 5, "Advantage player2")]
     [TestFixture(15, 14, "Advantage player1")]
     [TestFixture(14, 15, "Advantage player2")]
-    [TestFixture( 6,  4, "Win for player1")]
-    [TestFixture( 4,  6, "Win for player2")]
+    [TestFixture(6, 4, "Win for player1")]
+    [TestFixture(4, 6, "Win for player2")]
     [TestFixture(16, 14, "Win for player1")]
     [TestFixture(14, 16, "Win for player2")]
     public class TennisTests
@@ -66,9 +66,9 @@ namespace Tennis
                 if (i < this.player2Score)
                     game.WonPoint("player2");
             }
+
             Assert.AreEqual(this.expectedScore, game.GetScore());
         }
-
     }
 
     [TestFixture]
@@ -92,8 +92,9 @@ namespace Tennis
 
         private void RealisticTennisGame(ITennisGame game)
         {
-            string[] points = { "player1", "player1", "player2", "player2", "player1", "player1" };
-            string[] expectedScores = { "Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for player1" };
+            string[] points = {"player1", "player1", "player2", "player2", "player1", "player1"};
+            string[] expectedScores =
+                {"Fifteen-Love", "Thirty-Love", "Thirty-Fifteen", "Thirty-All", "Forty-Thirty", "Win for player1"};
             for (var i = 0; i < 6; i++)
             {
                 game.WonPoint(points[i]);
@@ -101,6 +102,4 @@ namespace Tennis
             }
         }
     }
-
 }
-
