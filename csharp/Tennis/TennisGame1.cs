fbfbd8d91dbbdb2b8dc656d7711a6d86cc7d1f30
Tom Stuttard <tom.e.stuttard@gmail.com>
2020-09-23 22:35:04 +0100

    Refactor TennisGame1.cs to use GameScore ValueObject

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,13 +2,12 @@ using System.Collections.Generic;
 
 namespace Tennis
 {
-    class TennisGame1 : ITennisGame
+    class GameScore
     {
         private string player1Name;
         private string player2Name;
         private Dictionary<string, int> score = new Dictionary<string, int>();
-
-        public TennisGame1(string player1Name, string player2Name)
+        public GameScore(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
             this.player2Name = player2Name;
@@ -16,17 +15,53 @@ namespace Tennis
             score[player2Name] = 0;
         }
 
-        public void WonPoint(string playerName)
+        public int getPlayerOneScore()
         {
-            // todo use map to track points won.
-            // todo use a ValueObject to represent a Point
-            
+            return score[player1Name];
+        }
+
+        public int getPlayerTwoScore()
+        {
+            return score[player2Name];
+        }
+
+        public void increaseScore(string playerName)
+        {
             if (!score.ContainsKey(playerName))
             {
                 throw new PlayerNotFoundException($"{playerName} not found.");
             }
-
+            
             score[playerName] += 1;
+        }
+
+        public bool isScoreTied()
+        {
+            return score[player1Name] == score[player2Name];
+        }
+
+        public bool isDeuce()
+        {
+            return score[player1Name] >= 4 || score[player2Name] >= 4;
+        }
+    }
+    
+    class TennisGame1 : ITennisGame
+    {
+        private string player1Name;
+        private string player2Name;
+        private GameScore currentGameScore;
+
+        public TennisGame1(string player1Name, string player2Name)
+        {
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
+            currentGameScore = new GameScore(player1Name, player2Name);
+        }
+
+        public void WonPoint(string playerName)
+        {
+            currentGameScore.increaseScore(playerName);
         }
 
         public string GetScore()
@@ -37,9 +72,9 @@ namespace Tennis
             // todo allow the changing of how the scores are displayed
             // todo show which players are winning
             // todo add a wimbledon scoreboard display
-            if (isScoreTied())
+            if (currentGameScore.isScoreTied())
             {
-                switch (score[player1Name])
+                switch (currentGameScore.getPlayerOneScore())
                 {
                     case 0:
                         scoreOutput = "Love-All";
@@ -56,9 +91,9 @@ namespace Tennis
 
                 }
             }
-            else if (isDuece())
+            else if (currentGameScore.isDeuce())
             {
-                var minusResult = score[player1Name] - score[player2Name];
+                var minusResult = currentGameScore.getPlayerOneScore() - currentGameScore.getPlayerTwoScore();
                 if (minusResult == 1) scoreOutput = "Advantage player1";
                 else if (minusResult == -1) scoreOutput = "Advantage player2";
                 else if (minusResult >= 2) scoreOutput = "Win for player1";
@@ -68,8 +103,8 @@ namespace Tennis
             {
                 for (var i = 1; i < 3; i++)
                 {
-                    if (i == 1) tempScore = score[player1Name];
-                    else { scoreOutput += "-"; tempScore = score[player2Name]; }
+                    if (i == 1) tempScore = currentGameScore.getPlayerOneScore();
+                    else { scoreOutput += "-"; tempScore = currentGameScore.getPlayerTwoScore(); }
                     switch (tempScore)
                     {
                         case 0:
@@ -88,16 +123,6 @@ namespace Tennis
                 }
             }
             return scoreOutput;
-        }
-
-        private bool isDuece()
-        {
-            return score[player1Name] >= 4 || score[player2Name] >= 4;
-        }
-
-        private bool isScoreTied()
-        {
-            return score[player1Name] == score[player2Name];
         }
     }
 }
